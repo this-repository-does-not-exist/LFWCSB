70b483a36f1bc4a3cd0bc0a4c62425cbbf1ab1bc
Kuba Werłos <werlos@gmail.com>
2023-08-07 18:00:59 +0200

    Implement Game

--- src/Game/Game.php
+++ src/Game/Game.php
@@ -2,4 +2,54 @@
 
 namespace App\Game;
 
-final class Game {}
+final class Game
+{
+    public function __construct(
+        private readonly string $homeTeam,
+        private readonly string $awayTeam,
+        private int $homeTeamScore = 0,
+        private int $awayTeamScore = 0,
+    ) {
+        if ($homeTeam === $awayTeam) {
+            throw new GameException('Cannot create a game with a team against themselves!');
+        }
+    }
+
+    public function updateScore(int $homeTeamScore, int $awayTeamScore): void
+    {
+        if ($homeTeamScore < $this->homeTeamScore) {
+            throw new GameException('Cannot decrease score of home team!');
+        }
+
+        if ($awayTeamScore < $this->awayTeamScore) {
+            throw new GameException('Cannot decrease score of away team!');
+        }
+
+        if ($homeTeamScore + $awayTeamScore === $this->homeTeamScore + $this->awayTeamScore) {
+            throw new GameException('Cannot update with the same scores!');
+        }
+
+        $this->homeTeamScore = $homeTeamScore;
+        $this->awayTeamScore = $awayTeamScore;
+    }
+
+    public function homeTeam(): string
+    {
+        return $this->homeTeam;
+    }
+
+    public function awayTeam(): string
+    {
+        return $this->awayTeam;
+    }
+
+    public function homeTeamScore(): int
+    {
+        return $this->homeTeamScore;
+    }
+
+    public function awayTeamScore(): int
+    {
+        return $this->awayTeamScore;
+    }
+}
--- tests/Game/GameTest.php
+++ tests/Game/GameTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Game;
 
 use App\Game\Game;
+use App\Game\GameException;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -10,11 +11,87 @@ use PHPUnit\Framework\TestCase;
  */
 final class GameTest extends TestCase
 {
-    public function testToHaveOneTest(): void
+    public function testCreatingGameWithTeamAgainstThemselves(): void
     {
-        $game1 = new Game();
-        $game2 = new Game();
+        $this->expectException(GameException::class);
+        $this->expectExceptionMessage('Cannot create a game with a team against themselves!');
 
-        self::assertNotSame($game1, $game2);
+        new Game('Arg', 'Arg');
+    }
+
+    public function testCreatingGame(): void
+    {
+        $game = new Game('Arg', 'Bra');
+
+        self::assertSame('Arg', $game->homeTeam());
+        self::assertSame('Bra', $game->awayTeam());
+        self::assertSame(0, $game->homeTeamScore());
+        self::assertSame(0, $game->awayTeamScore());
+    }
+
+    /**
+     * @dataProvider provideUpdatingScoreCorrectlyCases
+     */
+    public function testUpdatingScoreCorrectly(
+        int $homeTeamScoreBefore,
+        int $awayTeamScoreBefore,
+        int $homeTeamScoreAfter,
+        int $awayTeamScoreAfter,
+    ): void {
+        $game = new Game('Arg', 'Bra', $homeTeamScoreBefore, $awayTeamScoreBefore);
+
+        self::assertSame($homeTeamScoreBefore, $game->homeTeamScore());
+        self::assertSame($awayTeamScoreBefore, $game->awayTeamScore());
+
+        $game->updateScore($homeTeamScoreAfter, $awayTeamScoreAfter);
+
+        self::assertSame($homeTeamScoreAfter, $game->homeTeamScore());
+        self::assertSame($awayTeamScoreAfter, $game->awayTeamScore());
+    }
+
+    /**
+     * @return iterable<array{int, int, int, int}>
+     */
+    public static function provideUpdatingScoreCorrectlyCases(): iterable
+    {
+        yield [0, 0, 1, 0];
+        yield [0, 0, 0, 1];
+        yield [0, 0, 4, 3];
+        yield [5, 3, 5, 6];
+        yield [1, 7, 8, 7];
+    }
+
+    /**
+     * @dataProvider provideUpdatingScoreIncorrectlyCases
+     */
+    public function testUpdatingScoreIncorrectly(
+        int $homeTeamScoreBefore,
+        int $awayTeamScoreBefore,
+        int $homeTeamScoreAfter,
+        int $awayTeamScoreAfter,
+        string $exceptionMessage,
+    ): void {
+        $game = new Game('Arg', 'Bra', $homeTeamScoreBefore, $awayTeamScoreBefore);
+
+        self::assertSame($homeTeamScoreBefore, $game->homeTeamScore());
+        self::assertSame($awayTeamScoreBefore, $game->awayTeamScore());
+
+        $this->expectException(GameException::class);
+        $this->expectExceptionMessage($exceptionMessage);
+
+        $game->updateScore($homeTeamScoreAfter, $awayTeamScoreAfter);
+    }
+
+    /**
+     * @return iterable<array{int, int, int, int, string}>
+     */
+    public static function provideUpdatingScoreIncorrectlyCases(): iterable
+    {
+        yield [0, 0, 0, 0, 'Cannot update with the same scores!'];
+        yield [1, 0, 0, 0, 'Cannot decrease score of home team!'];
+        yield [5, 3, 1, 3, 'Cannot decrease score of home team!'];
+        yield [0, 1, 0, 0, 'Cannot decrease score of away team!'];
+        yield [1, 7, 2, 2, 'Cannot decrease score of away team!'];
+        yield [3, 3, 2, 2, 'Cannot decrease score of home team!'];
     }
 }
